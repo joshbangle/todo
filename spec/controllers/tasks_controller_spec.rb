c869f93c85df3791e0be31d449db8898277fa347
Joshua Bangle <joshua.bangle@gmail.com>
2019-10-12 22:21:18 +0000

    Added form to create new tasks

--- spec/controllers/tasks_controller_spec.rb
+++ spec/controllers/tasks_controller_spec.rb
@@ -18,13 +18,23 @@ RSpec.describe TasksController, type: :controller do
     end
   end
 
-  describe "task#update" do
+  describe "tasks#update" do
     it "should allow tasks to be marked as done" do
       task = FactoryBot.create(:task)
       get :update, params: {id: task.id, task: { done: true}}
       expect(response).to have_http_status(:success)
       task.reload
       expect(task.done).to eq(true)
+    end
+  end
+
+  describe "tasks#create" do
+    it "should allow new tasks to be created" do
+      post :create, params: {task: {title: "Fix things"}}
+      expect(response).to have_http_status(:success)
+      response_value = ActiveSupport::JSON.decode(@response.body)
+      expect(response_value['title']).to eq("Fix things")
+      expect(Task.last.title).to eq("Fix things")
     end
   end
 
